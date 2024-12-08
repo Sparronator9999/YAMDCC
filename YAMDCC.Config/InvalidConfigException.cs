58dbf667fa2e95779a5870ce4e72094502678e4d
Sparronator9999 <86388887+Sparronator9999@users.noreply.github.com>
2024-12-08 19:43:18 +1100

    Remove unneccesary exception code
    InvalidConfigException is used in exactly one place, and isn't meant to be used anywhere else (except in exception handling code)
    
    Oh yeah, and there was also a leftover using in FanRPMConf that got removed too

--- YAMDCC.Config/InvalidConfigException.cs
+++ YAMDCC.Config/InvalidConfigException.cs
@@ -15,48 +15,18 @@
 // YAMDCC. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace YAMDCC.Config
 {
     /// <summary>
     /// The exception thrown when an invalid <see cref="YAMDCC_Config"/> is loaded.
     /// </summary>
-    [Serializable]
     public sealed class InvalidConfigException : Exception
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="InvalidConfigException" /> class.
         /// </summary>
         public InvalidConfigException()
-            : base("The fan control config was not in an expected format.") { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InvalidConfigException"/>
-        /// class with a specified error message.
-        /// </summary>
-        /// <param name="message">
-        /// The message that describes the error.
-        /// </param>
-        public InvalidConfigException(string message)
-            : base(message) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InvalidConfigException"/>
-        /// class with a specified error message and a reference to the inner
-        /// exception that is the cause of this exception.
-        /// </summary>
-        /// <param name="message">
-        /// The error message that explains the reason for the exception.
-        /// </param>
-        /// <param name="innerException">
-        /// The exception that is the cause of the current exception,
-        /// or <c>null</c> if no inner exception is specified.
-        /// </param>
-        public InvalidConfigException(string message, Exception innerException)
-            : base(message, innerException) { }
-
-        private InvalidConfigException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
+            : base("The YAMDCC config was not in the expected format.") { }
     }
 }
