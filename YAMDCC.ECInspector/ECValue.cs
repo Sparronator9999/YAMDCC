2ec3266224fd001e5d820f44375d510b0ef9ba3a
Sparronator9999 <86388887+Sparronator9999@users.noreply.github.com>
2025-01-06 17:06:35 +1100

    Fix missing copyright/license headers in EC Monitor source files

--- YAMDCC.ECInspector/ECValue.cs
+++ YAMDCC.ECInspector/ECValue.cs
@@ -1,3 +1,19 @@
+// This file is part of YAMDCC (Yet Another MSI Dragon Center Clone).
+// Copyright © Sparronator9999 and Contributors 2025.
+//
+// YAMDCC is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// YAMDCC is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// YAMDCC. If not, see <https://www.gnu.org/licenses/>.
+
 namespace YAMDCC.ECInspector
 {
     internal struct ECValue
@@ -8,7 +24,7 @@ namespace YAMDCC.ECInspector
         public int Value;
 
         /// <summary>
-        /// How long it's been since <see cref="Value"/> was last updated.
+        /// How many EC polls it's been since <see cref="Value"/> was last updated.
         /// </summary>
         public int Age;
     }
