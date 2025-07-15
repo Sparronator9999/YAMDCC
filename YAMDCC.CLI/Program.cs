4ab9c48b161093ce57c478b0aec8a4b3d9e001fd
Sparronator9999 <86388887+Sparronator9999@users.noreply.github.com>
2025-07-16 06:31:54 +1000

    Initial command line argument parsing
    We still don't do anything yet, but it's a start...

--- YAMDCC.CLI/Program.cs
+++ YAMDCC.CLI/Program.cs
@@ -68,11 +68,179 @@ internal static class Program
         {
             Console.WriteLine("ERROR: no arguments");
             PrintHelp();
+            return;
         }
         else if (Args.ContainsKey("help") || Args.ContainsKey("H"))
         {
             PrintHelp();
+            return;
         }
+
+        bool error = false,
+            showInfo = false,
+            ecMonitor = false,
+            newFanProf = false,
+            delFanProf = false,
+            applyConf = false;
+
+        int fanIdx = -1,
+            profIdx = -1,
+
+            fanSpd = -1,
+            tUp = -1,
+            tDown = -1,
+            chargeLim = -1,
+            perfMode = -2,  // -1 is used as "default" in fan profile settings
+            keyLight = -1;
+
+        string fanName = string.Empty,
+            profName = string.Empty,
+            confPath = Paths.CurrentConf;
+
+        #region Parse actual commands and arguments
+        ConsoleColor fgColor = Console.ForegroundColor;
+        foreach ((string verb, string arg) in Args)
+        {
+            // Check for missing or unknown arguments
+            switch (verb)
+            {
+                case "F":
+                case "fan":
+                case "P":
+                case "profile":
+                case "S":
+                case "speed":
+                case "chargelim":
+                case "perfmode":
+                case "keylight":
+                case "C":
+                case "config":
+                case "author":
+                    if (string.IsNullOrEmpty(arg))
+                    {
+                        error = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: Missing argument for `{verb}`");
+                        Console.ForegroundColor = fgColor;
+                        continue;
+                    }
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"WARNING: Unknown argument `{verb}`");
+                    Console.ForegroundColor = fgColor;
+                    continue;
+            }
+
+            // actually parse arguments
+            switch (verb)
+            {
+                case "I":
+                case "info":
+                    showInfo = true;
+                    break;
+                case "M":
+                case "monitor":
+                    ecMonitor = true;
+                    break;
+                case "F":
+                case "fan":
+                    // Try to access fan by index first, otherwise
+                    // try accessing by name (done after parsing all arguments)
+                    if (!int.TryParse(arg, out fanIdx))
+                    {
+                        fanName = arg;
+                    }
+                    break;
+                case "P":
+                case "profile":
+                    // Same as "fan" but for fan profile.
+                    if (!int.TryParse(arg, out profIdx))
+                    {
+                        profName = arg;
+                    }
+                    break;
+                case "N":
+                case "new":
+                    delFanProf = false;
+                    newFanProf = true;
+                    break;
+                case "D":
+                case "delete":
+                    newFanProf = false;
+                    delFanProf = true;
+                    break;
+                case "S":
+                case "speed":
+                    string[] spdArgs = arg.Split(',');
+                    if (spdArgs.Length < 2 || spdArgs.Length > 4)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: Not enough arguments for `{verb}`");
+                        Console.WriteLine($"(expected 2-4, got {spdArgs.Length})");
+                        Console.ForegroundColor = fgColor;
+                        return;
+                    }
+                    if (!int.TryParse(spdArgs[0], out fanIdx) ||
+                       !int.TryParse(spdArgs[1], out fanSpd) ||
+                       (spdArgs.Length >= 3 && !int.TryParse(spdArgs[2], out tUp)) ||
+                       (spdArgs.Length >= 4 && !int.TryParse(spdArgs[3], out tDown)))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: failed to parse arguments for `{verb}`");
+                        Console.ForegroundColor = fgColor;
+                        return;
+                    }
+                    break;
+                case "chargelim":
+                    if (!int.TryParse(arg, out chargeLim))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: failed to parse argument for `{verb}`");
+                        Console.ForegroundColor = fgColor;
+                        return;
+                    }
+                    break;
+                case "perfmode":
+                    if (!int.TryParse(arg, out perfMode))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: failed to parse argument for `{verb}`");
+                        Console.ForegroundColor = fgColor;
+                        return;
+                    }
+                    break;
+                case "keylight":
+                    if (!int.TryParse(arg, out keyLight))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: failed to parse argument for `{verb}`");
+                        Console.ForegroundColor = fgColor;
+                        return;
+                    }
+                    break;
+                case "C":
+                case "config":
+                    confPath = arg;
+                    break;
+                case "A":
+                case "apply":
+                    applyConf = true;
+                    break;
+                case "author":
+                    break;
+            }
+        }
+
+        // stop here if we ran into command parsing errors
+        // (done so we can show multiple parsing errors at once)
+        if (error)
+        {
+            return;
+        }
+        #endregion
+
+
     }
 
     private static void PrintHelp()
@@ -114,7 +282,13 @@ internal static class Program
         if (!string.IsNullOrEmpty(verb))
         {
             Args.Add(verb, arg);
-            arg = string.Empty;
         }
+    }
+
+    // https://stackoverflow.com/a/42549535
+    public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+    {
+        key = tuple.Key;
+        value = tuple.Value;
     }
 }
